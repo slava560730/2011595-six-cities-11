23d9491a8200832a9287f9dab9d1cee65fe421bc
Петров Слава <pvv56@mail.ru>
2022-12-18 18:12:20 +0300

    deploy9

--- project/src/consts.ts
+++ project/src/consts.ts
@@ -19,7 +19,7 @@ export enum AuthorizationStatus {
 }
 
 export enum AppRoute {
-  Main = '/2011595-six-cities-11/',
+  Main = 'https://slava560730.github.io/2011595-six-cities-11/',
   Login = '/login',
   Favorites = '/favorites',
   Room = '/offer/:id',
